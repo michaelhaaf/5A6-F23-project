f99dc14ae23cc9a7b05d5f81dbf50e7d2b700836
Michael Haaf <michael.haaf@gmail.com>
2023-10-05 21:51:19 -0400

    feat: add SignIn screen, user interface
    Signed-off-by: Michael Haaf <michael.haaf@gmail.com>

---
+++ app/src/main/java/com/example/michaelappmilestone2/signin/SignInScreen.kt
@@ -0,0 +1,288 @@
+/*
+ * Copyright 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.compose.jetsurvey.signinsignup
+
+import android.content.res.Configuration.UI_MODE_NIGHT_NO
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FilledTonalButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.LocalContentColor
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.luminance
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.michaelappmilestone2.R
+import com.example.michaelappmilestone2.signin.EmailState
+import com.example.michaelappmilestone2.signin.EmailStateSaver
+import com.example.michaelappmilestone2.signin.PasswordState
+import com.example.michaelappmilestone2.signin.TextFieldState
+import com.example.michaelappmilestone2.ui.theme.ActivityTheme
+
+@Composable
+fun SignInScreen(
+    onSignIn: () -> Unit = {},
+    onCreateAccount: () -> Unit = {},
+    onContinueAsGuest: () -> Unit = {},
+) {
+    Surface() {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .verticalScroll(rememberScrollState()),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Banner()
+            Spacer(modifier = Modifier.height(16.dp))
+            SignInUI(onSignIn, onCreateAccount, onContinueAsGuest)
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class) // OutlinedTextField is experimental in m3
+@Composable
+fun EmailField(
+    emailState: TextFieldState = remember { EmailState() },
+) {
+    OutlinedTextField(
+        value = emailState.text,
+        onValueChange = {
+            emailState.text = it
+        },
+        label = {
+            Text(
+                text = stringResource(id = R.string.email),
+                style = MaterialTheme.typography.bodyMedium,
+            )
+        },
+        modifier = Modifier
+            .fillMaxWidth()
+            .onFocusChanged { focusState ->
+                emailState.onFocusChange(focusState.isFocused)
+                if (!focusState.isFocused) {
+                    emailState.enableShowErrors()
+                }
+            },
+        textStyle = MaterialTheme.typography.bodyMedium,
+        isError = emailState.showErrors(),
+    )
+    emailState.getError()?.let { error -> TextFieldError(textError = error) }
+}
+
+@OptIn(ExperimentalMaterial3Api::class) // OutlinedTextField is experimental in m3
+@Composable
+fun PasswordField(
+    passwordState: TextFieldState = remember { PasswordState() },
+    modifier: Modifier = Modifier,
+) {
+    val showPassword = rememberSaveable { mutableStateOf(false) }
+    OutlinedTextField(
+        value = passwordState.text,
+        onValueChange = {
+            passwordState.text = it
+            passwordState.enableShowErrors()
+        },
+        modifier = modifier
+            .fillMaxWidth()
+            .onFocusChanged { focusState ->
+                passwordState.onFocusChange(focusState.isFocused)
+                if (!focusState.isFocused) {
+                    passwordState.enableShowErrors()
+                }
+            },
+        textStyle = MaterialTheme.typography.bodyMedium,
+        label = {
+            Text(
+                text = stringResource(id = R.string.password),
+                style = MaterialTheme.typography.bodyMedium,
+            )
+        },
+        trailingIcon = {
+            if (showPassword.value) {
+                IconButton(onClick = { showPassword.value = false }) {
+                    Icon(
+                        imageVector = Icons.Filled.Visibility,
+                        contentDescription = stringResource(id = R.string.hide_password)
+                    )
+                }
+            } else {
+                IconButton(onClick = { showPassword.value = true }) {
+                    Icon(
+                        imageVector = Icons.Filled.VisibilityOff,
+                        contentDescription = stringResource(id = R.string.show_password)
+                    )
+                }
+            }
+        },
+        visualTransformation = if (showPassword.value) {
+            VisualTransformation.None
+        } else {
+            PasswordVisualTransformation()
+        },
+        isError = passwordState.showErrors(),
+        supportingText = {
+            passwordState.getError()?.let { error -> TextFieldError(textError = error) }
+        }
+    )
+}
+
+@Composable
+fun TextFieldError(textError: String) {
+    Row(modifier = Modifier.fillMaxWidth()) {
+        Spacer(modifier = Modifier.width(16.dp))
+        Text(
+            text = textError,
+            modifier = Modifier.fillMaxWidth(),
+            color = MaterialTheme.colorScheme.error
+        )
+    }
+}
+
+
+@Composable
+fun SignInUI(
+    onSignIn: () -> Unit = {},
+    onCreateAccount: () -> Unit = {},
+    onContinueAsGuest: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    val emailState by rememberSaveable(stateSaver = EmailStateSaver) {
+        mutableStateOf(EmailState())
+    }
+    val passwordState = remember { PasswordState() }
+
+    Column(
+        modifier = Modifier
+            .padding(horizontal = 20.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        EmailField()
+        PasswordField()
+        Button(
+            onClick = onSignIn,
+            modifier = Modifier
+                .fillMaxWidth(),
+            enabled = emailState.isValid && passwordState.isValid
+        ) {
+            Text(
+                text = stringResource(id = R.string.sign_in)
+            )
+        }
+        FilledTonalButton(
+            onClick = onSignIn,
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            Text(
+                text = stringResource(id = R.string.create_account)
+            )
+        }
+        OutlinedButton(
+            onClick = onSignIn,
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            Text(
+                text = stringResource(id = R.string.sign_in_guest),
+            )
+        }
+    }
+}
+
+@Composable
+fun Banner(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier.wrapContentHeight(align = Alignment.CenterVertically)
+    ) {
+        Logo(
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .padding(horizontal = 76.dp)
+        )
+        Text(
+            text = stringResource(id = R.string.app_tagline),
+            style = MaterialTheme.typography.titleMedium,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .padding(top = 24.dp)
+                .fillMaxWidth()
+        )
+    }
+}
+
+@Composable
+private fun Logo(
+    modifier: Modifier = Modifier,
+    lightTheme: Boolean = LocalContentColor.current.luminance() < 0.5f,
+) {
+    val assetId = if (lightTheme) {
+        R.drawable.logo_light
+    } else {
+        R.drawable.logo_dark
+    }
+    Image(
+        painter = painterResource(id = assetId),
+        modifier = modifier,
+        contentDescription = null
+    )
+}
+
+@Preview(name = "Welcome light theme", uiMode = UI_MODE_NIGHT_YES)
+@Preview(name = "Welcome dark theme", uiMode = UI_MODE_NIGHT_NO)
+@Composable
+fun SignInScreenPreview() {
+    ActivityTheme {
+        SignInScreen(
+            onSignIn = {},
+            onCreateAccount = {},
+            onContinueAsGuest = {},
+        )
+    }
+}
